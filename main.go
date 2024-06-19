18973298aa7e4e9bb0d055c047fab186f044e9c0
JetBrainsUser <48473447+jetbrainsuser@users.noreply.github.com>
2024-06-19 09:05:50 +0200

    Refactor main.go and update Person struct
    The main.go file is refactored to improve the structure and readability of the code. The Person struct has been updated to replace the Address field of type slice of strings, with a more expressive Location type. Also, the birthday function has been improved to directly print the birthday message.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 type Person struct {
 	Name    string
 	Age     int
-	Address []string
+	Address []Location
 }
 
 type Location struct {
@@ -16,37 +16,33 @@ type Location struct {
 	State  string
 }
 
-const cityIndex = 1
+const (
+	cityIndex      = 1
+	ageOfAdulthood = 18
+)
 
-func (person *Person) PrintDetails() {
-	cityName := person.Address[cityIndex]
+func main() {
+	person := Person{Name: "John Doe", Age: 25, Address: []Location{{Street: "123 Main St", City: "Anytown", State: "NY"}}}
+
+	if person.IsAdult() {
+		fmt.Printf("%s is an adult.\n", person.Name)
+	} else {
+		fmt.Printf("%s is not an adult.\n", person.Name)
+	}
+	person.CelebrateBirthday(person.Name)
+
+}
+
+func (person *Person) PrintPersonDetails() {
+	cityName := person.Address[cityIndex].City
 	fmt.Printf("Name: %s, Age: %d, City: %s\n", person.Name, person.Age, cityName)
 }
 
-const ageOfAdulthood = 18
-
-func (p *Person) celebrateBirthday() {
-	p.incrementAge()
-	fmt.Printf("Happy Birthday %s! You are now %d years old.\n", p.Name, p.Age)
-}
-
-func (p *Person) incrementAge() {
+func (p *Person) CelebrateBirthday(name string) {
 	p.Age += 1
+	fmt.Printf("Happy Birthday %s! You are now %d years old.\n", name, p.Age)
 }
 
 func (p *Person) IsAdult() bool {
 	return p.Age >= ageOfAdulthood
-}
-
-func main() {
-	person := Person{Name: "John Doe", Age: 25, Address: []string{"123 Main St", "Anytown", "NY"}}
-	PrintAdultStatus(person)
-}
-
-func PrintAdultStatus(person Person) {
-	message := fmt.Sprintf("%s is an adult.\n", person.Name)
-	if !person.IsAdult() {
-		message = fmt.Sprintf("%s is not an adult.\n", person.Name)
-	}
-	fmt.Print(message)
 }
